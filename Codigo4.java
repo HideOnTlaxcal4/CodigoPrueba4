70684d6ccf45aaab2fa05d9fe1043e46b0b1ae2b
eduardo <edu4yh@gmail.com>
2024-12-16 19:41:39 -0600

    Segundo commit: Añadimos bucle

--- Codigo4.java
+++ Codigo4.java
@@ -1,6 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int i = 0; // Posición para los elementos únicos
+
+        for (int num : nums) {
+            // La lógica de validación se añadirá después
+        }
+
         return 0; // Retorno temporal
     }
 }
